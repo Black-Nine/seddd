ed12a284d1deaf10b5d63825e546517a0e7a8f0f
TELE BN <neorabbit.m@gmail.com>
2016-09-13 11:55:45 +0430

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -1,5 +1,21 @@
 do
 
+function run(msg, matches)
+    return "سلف بات v1 \nby Lammer Team :) <3 \n@LammerTeam \n@MrLammer"
+end 
+return {
+  patterns = {
+    "^#version$",
+    "^#bot$",
+    "^#LammerTeam$",
+    "^#selfbot$"
+  },
+  run = run
+}
+end
+
+do
+
 local function run(msg, matches)
       local text = "Beyond Self Bot V1.0\nAn Fun Bot Based On TeleSeed Written In Lua\n\nSudo User :\nDeveloper&Founder : @SoLiD021\nDeVeloper&Manager : @CliApi\n\nTeam Channel :\n@BeyondTeam\n\nSpecial Thx To :\nSeed Team\nAnd All My Friends :D\n\nBeyond Self Bot Version 1.0 On GitHub :\nGithub.com/BeyondTeam/Self-Bot"
   if matches[1]:lower() == 'beyondself' or 'version' or 'ver' or 'git' then --change this with anything you want
